6eac963edb30148917ea2c790114adc7aaf707fb
Ricardo Díaz <49500398+richardd82@users.noreply.github.com>
2025-04-29 02:20:50 -0600

    Change source for userData info in Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -67,7 +67,7 @@ const Dashboard = () => {
 //className="min-h-screen flex flex-col items-center justify-center"
   return (
     <div className='mt-10 justify-center items-center'>
-      <h1 className="text-3xl font-bold mb-4">Bienvenido, {`${user?.first_name} ${user?.last_name}`}</h1>
+      <h1 className="text-3xl font-bold mb-4">Bienvenido, {userData ? `${userData.first_name} ${userData.last_name}` : "Cargando..."}</h1>
 
       {error && <p className="text-red-500">{error}</p>}
 
